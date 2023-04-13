d013d57d0461c53a24f78b9c400c93261b092f62
Cihan Korkmaz <cihankorkmaz@windowslive.com>
2023-04-13 14:07:43 +0300

    Add X

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ const Board = (function Gameboard() {
 function Player() {
     return {}
 }
+
+function put() {
+    const para = document.createElement("p");
+    para.classList.add("p1");
+    const title = document.createTextNode("X");
+    para.appendChild(title);
+    const element = document.getElementById("div1");
+    element.insertBefore(para, element.firstChild);
+    const bt = document.getElementById("b1");
+    bt.parentNode.removeChild(bt);
+}
