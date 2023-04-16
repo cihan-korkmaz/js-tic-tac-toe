5887a506c6cbaec29660a35c158ee2b4fbb7bb8f
Cihan Korkmaz <cihankorkmaz@windowslive.com>
2023-04-16 13:30:13 +0300

    Add victory test and create Game module

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 const Board = (function Gameboard() {
-    let gameboard = []
-    return {gameboard}
+    let gameboard = [
+        [[], [], []], 
+        [[], [], []], 
+        [[], [], []]
+    ]
+    let icon_strike = "XXX"
+
+    function victory_test() {
+        let row_1 = gameboard[0][0] + gameboard[0][1] + gameboard[0][2];
+        let row_2 = gameboard[1][0] + gameboard[1][1] + gameboard[1][2];
+        let row_3 = gameboard[2][0] + gameboard[2][1] + gameboard[2][2];
+        let column_1 = gameboard[0][0] + gameboard[1][0] + gameboard[2][0];
+        let column_2 = gameboard[0][1] + gameboard[1][1] + gameboard[2][1];
+        let column_3 = gameboard[0][2] + gameboard[1][2] + gameboard[2][2];
+        let diagonal_1 = gameboard[0][0] + gameboard[1][1] + gameboard[2][2];
+        let diagonal_2 = gameboard[0][2] + gameboard[1][1] + gameboard[2][0];
+        
+        if (row_1 === icon_strike) {
+            console.log("X wins!");
+        } else if (row_2 === icon_strike) {
+            console.log("X wins!");
+        } else if (row_3 === icon_strike) {
+            console.log("X wins!");
+        } else if (column_1 === icon_strike) {
+            console.log("X wins!");
+        } else if (column_2 === icon_strike) {
+            console.log("X wins!");
+        } else if (column_3 === icon_strike) {
+            console.log("X wins!");
+        } else if (diagonal_1 === icon_strike) {
+            console.log("X wins!");
+        } else if (diagonal_2 === icon_strike) {
+            console.log("X wins!");
+        }
+    };
+
+    return {gameboard, victory_test}
 })();
 
-function Player() {
+const Game = (function Play() {
+    function put(id) {
+        const para = document.createElement("p");
+        para.classList.add("p1");
+        const x_icon = document.createTextNode("X");
+        para.appendChild(x_icon);
+        const bt = document.getElementById(id);
+        bt.parentNode.replaceChild(para, bt);
+    
+        Board.gameboard[parseInt(id[1])][parseInt(id[2])] = "X";
+        console.table(Board.gameboard);
+        Board.victory_test();
+        
+    }
     return {}
+})()
+
+function Player(icon, name) {
+    let wins;
+    return {icon, name, wins}
 }
 
-function put() {
-    const para = document.createElement("p");
-    para.classList.add("p1");
-    const title = document.createTextNode("X");
-    para.appendChild(title);
-    const element = document.getElementById("div1");
-    element.insertBefore(para, element.firstChild);
-    const bt = document.getElementById("b1");
-    bt.parentNode.removeChild(bt);
-}
+let player_x = Player()
+let player_y = Player()
+
